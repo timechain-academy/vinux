30efff9e10f10baac4c3cddc714a8389bd78fd3b
randymcmillan <randymcmillan@protonmail.com>
2025-12-17 14:52:11 -0500

    damus/Nostr/NostrKind.swift

--- damus/Nostr/NostrKind.swift
+++ damus/Nostr/NostrKind.swift
@@ -7,7 +7,6 @@
 
 import Foundation
 
-
 enum NostrKind: Int {
     case metadata = 0
     case text = 1
@@ -19,4 +18,12 @@ enum NostrKind: Int {
     case channel_create = 40
     case channel_meta = 41
     case chat = 42
+    case repository_announcement = 30617
+    case repository_state_announcement = 30618
+    case repository_reply = 1111
+    case repository_patch = 1617
+    case repository_issue_open = 1630
+    case repository_issue_applied = 1631
+    case repository_issue_closed = 1632
+    case repository_issue_draft = 1633
 }
