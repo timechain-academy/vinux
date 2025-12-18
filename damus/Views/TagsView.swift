17eb705c2b0ff1d0189dcfc8aa31f017d8df4538
randymcmillan <randymcmillan@protonmail.com>
2025-12-17 21:22:25 -0500

    damus/Views/TagsView.swift

--- damus/Views/TagsView.swift
+++ damus/Views/TagsView.swift
@@ -10,18 +10,78 @@ import SwiftUI
 struct TagsView: View {
     let tags: [[String]]
 
+    @State private var totalHeight
+          = CGFloat.zero       // << variant for VStack
+    //    = CGFloat.infinity   // << variant for LazyVStack
+
     var body: some View {
-        HStack {
-            ForEach(tags, id: \.self) { tag in
-                if tag.count > 1 {
-                    Text("\(tag[0]): \(tag[1])")
-                        .font(.footnote)
-                        .padding(.horizontal, 8)
-                        .padding(.vertical, 4)
-                        .background(Color.gray.opacity(0.2))
-                        .cornerRadius(8)
-                }
+        VStack {
+            GeometryReader { geometry in
+                self.generateContent(in: geometry)
             }
+        }
+        .frame(height: totalHeight)// << variant for VStack
+        //.frame(maxHeight: totalHeight) // << variant for LazyVStack
+    }
+
+    private func generateContent(in g: GeometryProxy) -> some View {
+        var width = CGFloat.zero
+        var height = CGFloat.zero
+
+        return ZStack(alignment: .topLeading) {
+            ForEach(self.tags, id: \.self) { tag in
+                self.item(for: tag)
+                    .padding([.horizontal, .vertical], 4)
+                    .alignmentGuide(.leading, computeValue: { d in
+                        if (abs(width - d.width) > g.size.width)
+                        {
+                            width = 0
+                            height -= d.height
+                        }
+                        let result = width
+                        if tag == self.tags.last! {
+                            width = 0 //last item
+                        } else {
+                            width -= d.width
+                        }
+                        return result
+                    })
+                    .alignmentGuide(.top, computeValue: {d in
+                        let result = height
+                        if tag == self.tags.last! {
+                            height = 0 // last item
+                        }
+                        return result
+                    })
+            }
+        }.background(viewHeightReader($totalHeight))
+    }
+
+    private func item(for tag: [String]) -> some View {
+        if tag.count > 1 {
+            return Text("\(tag[0]): \(tag[1])")
+                .font(.footnote)
+                .padding(.horizontal, 8)
+                .padding(.vertical, 4)
+                .background(Color.gray.opacity(0.2))
+                .cornerRadius(8)
+        } else {
+            return Text(tag[0])
+                .font(.footnote)
+                .padding(.horizontal, 8)
+                .padding(.vertical, 4)
+                .background(Color.gray.opacity(0.2))
+                .cornerRadius(8)
+        }
+    }
+
+    private func viewHeightReader(_ binding: Binding<CGFloat>) -> some View {
+        return GeometryReader { geometry -> Color in
+            let rect = geometry.frame(in: .local)
+            DispatchQueue.main.async {
+                binding.wrappedValue = rect.size.height
+            }
+            return .clear
         }
     }
 }
