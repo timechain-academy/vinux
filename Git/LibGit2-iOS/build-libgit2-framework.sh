fc012ea3e87517e557999307d66e589c923888b3
randymcmillan <randymcmillan@protonmail.com>
2025-12-20 14:39:54 -0500

    build-libgit2-framework.sh:intermediate

--- Git/LibGit2-iOS/build-libgit2-framework.sh
+++ Git/LibGit2-iOS/build-libgit2-framework.sh
@@ -16,10 +16,10 @@ export PATH=$PATH:$REPO_ROOT/tools/bin
 AVAILABLE_PLATFORMS=(iphoneos iphonesimulator iphonesimulator-arm64 maccatalyst maccatalyst-arm64) # macosx macosx-arm64
 
 # List of frameworks included in the XCFramework (= AVAILABLE_PLATFORMS without architecture specifications)
-XCFRAMEWORK_PLATFORMS=iphoneos
+XCFRAMEWORK_PLATFORMS=(iphoneos iphonesimulator maccatalyst)
 
 # List of platforms that need to be merged using lipo due to presence of multiple architectures
-LIPO_PLATFORMS=(maccatalyst)
+LIPO_PLATFORMS=(iphonesimulator maccatalyst)
 
 ### Setup common environment variables to run CMake for a given platform
 ### Usage:      setup_variables PLATFORM
@@ -97,7 +97,7 @@ function build_libpcre() {
 	git clone https://github.com/light-tech/PCRE.git pcre-8.45 || true
 	cd pcre-8.45
 
-	mkdir -p build && cd build
+	rm -rf build && mkdir build && cd build
 	CMAKE_ARGS+=(-DPCRE_BUILD_PCRECPP=NO \
 		-DPCRE_BUILD_PCREGREP=NO \
 		-DPCRE_BUILD_TESTS=NO \
@@ -105,9 +105,9 @@ function build_libpcre() {
                 -DCMAKE_POLICY_VERSION_MINIMUM=3.5 \
 		-DPCRE_SUPPORT_LIBBZ2=NO)
 
-		cmake "${CMAKE_ARGS[@]}" .. 
+	cmake "${CMAKE_ARGS[@]}" .. >/dev/null 2>/dev/null
 
-		cmake --build . --target install
+	cmake --build . --target install >/dev/null 2>/dev/null
 }
 
 ### Build openssl for a given platform
@@ -146,8 +146,8 @@ function build_openssl() {
 		--openssldir=$REPO_ROOT/install/$PLATFORM \
 		$TARGET_OS no-shared no-dso no-hw no-engine >/dev/null 2>/dev/null
 
-	make 
-	make install_sw install_ssldirs
+	make >/dev/null 2>/dev/null
+	make install_sw install_ssldirs >/dev/null 2>/dev/null
 	export -n CFLAGS
 }
 
@@ -160,7 +160,7 @@ function build_libssh2() {
 	tar xzf libssh2-1.10.0.tar.gz
 	cd libssh2-1.10.0
 
-	mkdir -p build && cd build
+	rm -rf build && mkdir build && cd build
 
 	CMAKE_ARGS+=(-DCRYPTO_BACKEND=OpenSSL \
 		-DOPENSSL_ROOT_DIR=$REPO_ROOT/install/$PLATFORM \
@@ -185,7 +185,7 @@ function build_libgit2() {
     ditto -V -x -k --sequesterRsrc --rsrc v1.3.1.zip ./ >/dev/null 2>/dev/null
     cd libgit2-1.3.1
 
-    mkdir -p build && cd build
+    rm -rf build && mkdir build && cd build
 
     CMAKE_ARGS+=(-DBUILD_CLAR=NO)
 
@@ -224,8 +224,7 @@ function build_xcframework() {
 ### Copy SwiftGit2's module.modulemap to libgit2.xcframework/*/Headers
 ### so that we can use libgit2 C API in Swift (e.g. via SwiftGit2)
 function copy_modulemap() {
-    local FWNAME=$1
-    local FWDIRS=$(find $FWNAME.xcframework -mindepth 1 -maxdepth 1 -type d)
+    local FWDIRS=$(find Clibgit2.xcframework -mindepth 1 -maxdepth 1 -type d)
     for d in ${FWDIRS[@]}; do
         echo $d
         cp Clibgit2_modulemap $d/Headers/module.modulemap
@@ -253,9 +252,9 @@ for p in ${LIPO_PLATFORMS[@]}; do
     test -f libgit2_all_archs.a && rm libgit2.a && mv libgit2_all_archs.a libgit2.a
 done
 
-## # Build raw libgit2 XCFramework for Objective-C usage
-## build_xcframework libgit2 ${XCFRAMEWORK_PLATFORMS[@]}
-## zip -r libgit2.xcframework.zip libgit2.xcframework/
+# Build raw libgit2 XCFramework for Objective-C usage
+build_xcframework libgit2 ${XCFRAMEWORK_PLATFORMS[@]}
+zip -r libgit2.xcframework.zip libgit2.xcframework/
 
 # Build Clibgit2 XCFramework for use with SwiftGit2
 build_xcframework Clibgit2 ${XCFRAMEWORK_PLATFORMS[@]}
